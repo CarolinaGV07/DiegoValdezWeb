0bf2cc6fb7c5565607e2bcf65c2eba67f515b708
Carolina GV <caro_gv2004@hotmail.com>
2024-08-13 21:56:22 -0300

    Cuarto commit

--- script.js
+++ script.js
@@ -83,4 +83,21 @@ function switchImage(currentImage){
 }
 */
 
+let menuVisible = false;
 
+//Funcion que oculta o muestra el menú
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+//Oculto el menu una vez que selecciono una opción
+function seleccionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
